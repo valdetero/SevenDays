ea55150f52975ddb591696d2dfdc69d1fd5b2b73
Seth Valdetero <valdetero@users.noreply.github.com>
2016-01-11 15:52:32 -0600

    Missing file for build server.

---
+++ SevenDays.Core/Helpers/ApiConstants.Keys.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenDays.Core
+{
+    public partial class ApiConstants
+    {
+    }
+}
